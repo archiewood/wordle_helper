0926555b77f3eca866714635e50475a54251a880
archie <archiewood@outlook.com>
2022-02-03 03:06:52 -0500

    adding imports to wsgi

--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,15 @@
 #whereas this file seems to be where you put any code / functions
+import dash
+from dash import dcc, html, dash_table, Input, Output
+import dash_bootstrap_components as dbc
+import plotly.express as px
+import pandas as pd
+import logging
+from app.data_operations import *
+
+
 from app.main import app
- 
+
 if __name__ == "__main__":
         app.run_server(debug=True)
 
