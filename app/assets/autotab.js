975cf2683a561de60fafb645878396c4c61c8830
archie <archiewood@outlook.com>
2022-03-06 23:33:45 -0500

    adding js to autotab between letters

---
+++ app/assets/autotab.js
@@ -0,0 +1,25 @@
+//define array for the entry boxes, and get a matrix of the IDs
+let box = [];
+
+for (let i = 0; i < 5; i++) {
+    box[i] = [];
+    for (let j = 1; j <= 5; j++) {
+        box[i][j] = { "component": "guess_input", "guess_number": j, "letter_position": i };
+    }
+}
+
+//need to wait for the DOM to load (some weird react js behaviour means that window.onload doesn't work), so keep checking until last box is loaded.
+function loadfunc() {
+    if (document.getElementById(JSON.stringify(box[4][5])) == null) {
+        window.requestAnimationFrame(loadfunc);
+    } else {
+        for (let i = 0; i < 4; i++) {
+            for (let j = 1; j <= 5; j++) {
+                document.getElementById(JSON.stringify(box[i][j])).addEventListener("keyup", function () { if (this.value.length > 0) { document.getElementById(JSON.stringify(box[i + 1][j])).focus() } });
+            }
+        }
+    }
+};
+
+
+loadfunc();
