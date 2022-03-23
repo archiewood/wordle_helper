88b2285198e8a922df2d9db5fbdea578c9ba356d
archie <archiewood@outlook.com>
2022-03-23 16:07:37 -0400

    added segment tracking

--- app/main.py
+++ app/main.py
@@ -10,7 +10,7 @@ import logging
 from app.data_operations import *
 
 meta_tags = [
-    {   "name": "viewport",
+    {"name": "viewport",
         "content": "width=device-width, initial-scale=1"
      },
     {
@@ -73,6 +73,12 @@ app.index_string = '''<!DOCTYPE html>
             gtag('js', new Date());
             
             gtag('config', 'G-1XTZ6F0JH1', { 'anonymize_ip': false });
+        </script>
+        <script>
+            !function(){var analytics=window.analytics=window.analytics||[];if(!analytics.initialize)if(analytics.invoked)window.console&&console.error&&console.error("Segment snippet included twice.");else{analytics.invoked=!0;analytics.methods=["trackSubmit","trackClick","trackLink","trackForm","pageview","identify","reset","group","track","ready","alias","debug","page","once","off","on","addSourceMiddleware","addIntegrationMiddleware","setAnonymousId","addDestinationMiddleware"];analytics.factory=function(e){return function(){var t=Array.prototype.slice.call(arguments);t.unshift(e);analytics.push(t);return analytics}};for(var e=0;e<analytics.methods.length;e++){var key=analytics.methods[e];analytics[key]=analytics.factory(key)}analytics.load=function(key,e){var t=document.createElement("script");t.type="text/javascript";t.async=!0;t.src="https://cdn.segment.com/analytics.js/v1/" + key + "/analytics.min.js";var n=document.getElementsByTagName("script")[0];n.parentNode.insertBefore(t,n);analytics._loadOptions=e};analytics._writeKey="5Sc81VlUXwW6TO3EuJbW7MBb8DdSxJCw";;analytics.SNIPPET_VERSION="4.15.3";
+            analytics.load("5Sc81VlUXwW6TO3EuJbW7MBb8DdSxJCw");
+            analytics.page();
+            }}();
         </script>
         {%metas%}
         <title>{%title%}</title>
@@ -162,8 +168,7 @@ app.layout = html.Div(
 
                     dbc.Col(children=[
                         dcc.Markdown(
-                            children=
-                            '''
+                            children='''
                             Enter word guesses and colors below to see possible solutions. 
                             
                             **NEW:** See which letters are most common in those solutions below.
